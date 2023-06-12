127d1b611eced021bfeb0e3ea3fe744fe5b59a1e
Anthony GOMEZ <anthony.gomez@akt.io>
2023-06-12 23:20:08 +0200

    try fix PWA registration

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,20 +1,20 @@
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
     event.waitUntil(
-      caches.open('poker-whispers').then(function(cache) {
-        return cache.addAll([
-          '/',
-          'css/style.css',
-          'img/logo.png',
-          'js/script.js'
-        ]);
-      })
+        caches.open('poker-whispers').then(function (cache) {
+            return cache.addAll([
+                '/',
+                'css/style.css',
+                'img/logo.png',
+                'js/script.js'
+            ]);
+        })
     );
-  });
-  
-  self.addEventListener('fetch', function(event) {
+});
+
+self.addEventListener('fetch', function (event) {
     event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
     );
-  });
+});
